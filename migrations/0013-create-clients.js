f64f51613c5ff095ba1932bd9cbc0a5fac2885cb
Bill Piedra <Bill@Bills-iMac-770.local>
2021-08-21 10:54:53 -0400

    fix

--- migrations/0013-create-clients.js
+++ migrations/0013-create-clients.js
@@ -22,6 +22,6 @@ module.exports = {
         });
     },
     down: (queryInterface, Sequelize) => {
-        return queryInterface.dropTable('Associations');
+        return queryInterface.dropTable('Clients');
     }
 };
